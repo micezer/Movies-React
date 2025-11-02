6619d108c1a5a370c0a10e89928cf91a257b95ce
micezer <obiezeh99@gmail.com>
2025-11-02 02:16:12 +0100

    Cuarto commit

--- src/components/MovieList/MovieDetails.jsx
+++ src/components/MovieList/MovieDetails.jsx
@@ -7,6 +7,9 @@ const MovieDetails = () => {
   const key = import.meta.env.VITE_API_KEY;
 
   const url = `https://api.themoviedb.org/3/movie/${params.id}?api_key=${key}`;
+  const image = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : null;
 
   useEffect(() => {
     async function fectchMovies() {
@@ -22,7 +25,36 @@ const MovieDetails = () => {
   });
   return (
     <main className="container">
-      <h5 className="card-title text-primary text-overflow-1">{movie.title}</h5>
+      <h5 className="text-danger py-2 border-bottom mb-3">{movie.title}</h5>
+
+      <div className="row">
+        <div className="col-md-4">
+          <img src={image} className="img-fluid img-thumbnail" />
+        </div>
+        <div className="col-md-8">
+          <h3 className="text-primary">{movie.title}</h3>
+          <p className="mt-3">{movie.overview}</p>
+
+          {movie.genres ? (
+            <p className="d-flex gap-3">
+              {movie.genres.map((genre) => (
+                <span key={genre.id} className="badge bg-danger">
+                  {genre.name}
+                </span>
+              ))}
+            </p>
+          ) : (
+            ""
+          )}
+
+          <p className="mt-2">
+            <i className="bi bi-star-fill text-warning"></i>
+            {movie.vote_average} |
+            <i className="bi bi-people-fill text-success"></i>
+            {movie.vote_count} Reviews
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
